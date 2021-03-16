3229a8976af1b65487c925dca2378c61c5305a0f
Carolyn Savas <carolyn.savas.keith@gmail.com>
2021-03-16 15:07:02 -0500

    Add files via upload

---
+++ Pancakes.java
@@ -0,0 +1,43 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Stack;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class Pancakes
+{
+ private Map<String,Stack> famPan;
+ private Stack<Integer> stack;
+ 
+ public Pancakes()
+ {
+   
+ }
+
+ public void putEntry(String entry)
+ {
+   //takes in data and adds to map w/ stack
+ }
+
+ public void makeStack(int w){
+   //takes in weight and creates stack
+   }
+ }
+ public Integer famToStack(String mem){
+ //input family member get stack value back
+   return 0;
+ }
+ 
+ public String eatPan(String mem, int eaten){
+   //i suggest returning both strings "person ate # pancakes" and "person's pancake stack: #"
+   return "";
+ }
+//make a toString
+}
---
+++ PancakesRunner.java
@@ -0,0 +1,18 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+
+public class PancakesRunner
+{
+ public static void main ( String[] args ) throws IOException
+ {
+  //get data from file w/ scanner
+   
+ }
+ }
